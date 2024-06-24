57c54ca7ae299d9213b4fab496b737aabcfcee22
JetTunique09 <noemie.rosen1@icloud.com>
2024-06-24 09:41:04 +0200

    First commit

---
+++ Java/demo-dfs1/src/Principale.java
@@ -0,0 +1,8 @@
+public class Principale {
+    public static void main(String[] args) {
+        int age = 21;
+        System.out.println("Hello Tess!");
+        System.out.println(age);
+
+    }
+}
